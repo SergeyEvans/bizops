5a371a2dee8723f094db42181e29f44da429f56f
mpg3200 <sergey123@gmail.com>
2017-08-02 17:26:02 +0300

    to do v03

--- js.js
+++ js.js
@@ -1,5 +1,28 @@
   $( function() {
     $( "#sortable1, #sortable2, #sortable3, #sortable4" ).sortable({
-      connectWith: ".connectedSortable"
+      connectWith: ".connectedSortable",
+      stop: function (event, ui) {
+          var data = $(this).sortable('serialize');
+        }
     }).disableSelection();
   } );
+
+
+var order = localStorage.setItem({'order':'test'})
+
+var sPositions = localStorage.positions || "{}",
+    positions = JSON.parse(sPositions);
+$.each(positions, function (id, pos) {
+    $("#" + id).css(pos)
+})
+
+$("#draggable3").draggable({
+    containment: "#containment-wrapper",
+    scroll: false,
+    stop: function (event, ui) {
+        positions[this.id] = ui.position
+        localStorage.positions = JSON.stringify(positions)
+    }
+});
+
+
